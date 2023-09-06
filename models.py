f61fdbe3a4363cf90ba41548e3dc09be41d180b3
AV2001 <anirudhvadlamani2001@gmail.com>
2023-09-06 08:39:55 +0400

    create Cupcake model

--- models.py
+++ models.py
@@ -3,6 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class Cupcake:
+class Cupcake(db.Model):
     '''Cupcake Model'''
-    ...
+
+    __tablename__ = 'cupcakes'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False,
+                      default='https://tinyurl.com/demo-cupcake')
