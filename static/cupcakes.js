1779db563d85dd6ea29235ba46e43d6ce0f42263
AV2001 <anirudhvadlamani2001@gmail.com>
2023-09-08 11:37:04 +0400

    process form data and send post request to add and show new cupcake

--- static/cupcakes.js
+++ static/cupcakes.js
@@ -8,4 +8,35 @@ async function getAllCupcakes() {
     }
 }
 
+$('form').submit(async (e) => {
+    e.preventDefault();
+    let flavor = $('#flavor');
+    let size = $('#size');
+    let rating = $('#rating');
+    let image = $('#image');
+    const response = await axios.post(
+        '/api/cupcakes',
+        {
+            flavor: flavor.val(),
+            size: size.val(),
+            rating: rating.val(),
+            image: image.val(),
+        },
+        {
+            headers: {
+                'content-type': 'application/json',
+            },
+        }
+    );
+    const cupcake = response.data['cupcake'];
+    $('.cupcakes-list').append(
+        `<li>${cupcake.flavor} <button class="remove-cupcake">X</button></li>`
+    );
+
+    flavor.val('');
+    size.val('');
+    rating.val('');
+    image.val('');
+});
+
 window.onload = getAllCupcakes;
